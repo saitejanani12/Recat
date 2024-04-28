a958738e26d8a41b00d99dfe9aad4928281869c5
saiteja <saitejaguntupalli296@gmail.com>
2024-04-28 09:24:52 +0530

    third commit

--- day1.js
+++ day1.js
@@ -1,6 +1,10 @@
-let div_tag = React.createElement('div',{id:'main_div'},
-React.createElement('div',{id:'inner_div'},
-React.createElement('h1',{id:'Main_heading'},'I Love Python and Django Framework'),
-React.createElement('h2',{id:'inner_h2'},
-React.createElement('b',{},'I am Sai teja')),));
-ReactDOM.createRoot(document.body).render(div_tag)
+// let div_tag = React.createElement('div',{id:'main_div'},
+// React.createElement('div',{id:'inner_div'},
+// React.createElement('h1',{id:'Main_heading'},'I Love Python and Django Framework'),
+// React.createElement('h2',{id:'inner_h2'},
+// React.createElement('b',{},'I am Sai teja')),));
+// ReactDOM.createRoot(document.body).render(div_tag)
+let parent_div = React.createElement('div',{id:'main_div'},
+[React.createElement('h1',{},'I love Python & Django'),React.createElement('h2',{},'I love Javascript & React ')
+])
+ReactDOM.createRoot(document.body).render(parent_div)
