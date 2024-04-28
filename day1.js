6a5ef53fe6bad062ce261109108846da2771e1e5
saiteja <saitejaguntupalli296@gmail.com>
2024-04-28 09:18:47 +0530

    secoun

--- day1.js
+++ day1.js
@@ -1,4 +1,6 @@
 let div_tag = React.createElement('div',{id:'main_div'},
 React.createElement('div',{id:'inner_div'},
-React.createElement('h1',{id:'Main_heading'},'I Love Python and Django Framework')));
+React.createElement('h1',{id:'Main_heading'},'I Love Python and Django Framework'),
+React.createElement('h2',{id:'inner_h2'},
+React.createElement('b',{},'I am Sai teja')),));
 ReactDOM.createRoot(document.body).render(div_tag)
