e773f77d4ce765bc2fa2bbb66c0e6e545ccd4f80
saiteja <saitejaguntupalli296@gmail.com>
2024-04-28 09:12:20 +0530

    create element by react

---
+++ day1.js
@@ -0,0 +1,4 @@
+let div_tag = React.createElement('div',{id:'main_div'},
+React.createElement('div',{id:'inner_div'},
+React.createElement('h1',{id:'Main_heading'},'I Love Python and Django Framework')));
+ReactDOM.createRoot(document.body).render(div_tag)
